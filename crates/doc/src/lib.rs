f6e7f3ba555a52215d24506ae8bbc4c8006918e8
Bhargav Kantheti <k@bk.bio>
2026-07-24 17:14:45 -0500

    MV domain out

--- crates/doc/src/lib.rs
+++ crates/doc/src/lib.rs
@@ -1,15 +1,10 @@
+mod domain;
 mod error;
-mod platform;
-mod product;
 mod response;
-mod updates;
 
-use std::borrow::Cow;
-
+pub use domain::Domain;
 pub use error::Error;
-pub use response::Documentation;
-
-const DOCS_HOME: &str = "https://www.palantir.com/docs/foundry/";
+pub use response::{Documentation, Link, NavItem as Item, Page};
 
 pub struct Client {
     http: reqwest::Client,
@@ -29,9 +24,9 @@ impl Client {
         let scope: Vec<&str> = scope.iter().map(AsRef::as_ref).collect();
 
         match domain {
-            Domain::Product => product::get(self, &scope).await,
-            Domain::Platform => platform::get(self, &scope).await,
-            Domain::Updates => updates::get(self, &scope).await,
+            Domain::Product => domain::product::get(self, domain.route().as_ref(), &scope).await,
+            Domain::Platform => domain::platform::get(self, domain.route().as_ref(), &scope).await,
+            Domain::Updates => domain::updates::get(self, domain.route().as_ref(), &scope).await,
         }
     }
 }
@@ -39,40 +34,5 @@ impl Client {
 impl Default for Client {
     fn default() -> Self {
         Self::new()
-    }
-}
-
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-pub enum Domain {
-    Product,
-    Platform,
-    Updates,
-}
-
-impl Domain {
-    pub const ALL: [Self; 3] = [Self::Product, Self::Platform, Self::Updates];
-
-    pub const fn name(self) -> &'static str {
-        match self {
-            Self::Product => "product",
-            Self::Platform => "platform",
-            Self::Updates => "updates",
-        }
-    }
-
-    pub const fn description(self) -> &'static str {
-        match self {
-            Self::Product => "Palantir Foundry products' documentation",
-            Self::Platform => "Palantir Foundry platform-as-API reference",
-            Self::Updates => "Palantir Foundry platform updates",
-        }
-    }
-
-    pub fn route(self) -> Cow<'static, str> {
-        match self {
-            Self::Product => DOCS_HOME.into(),
-            Self::Platform => format!("{DOCS_HOME}api/v2/").into(),
-            Self::Updates => format!("{DOCS_HOME}announcements/").into(),
-        }
     }
 }
