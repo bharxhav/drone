9d8e8bb84881c9d86c5ffeff94d25ec6fe9266a9
Bhargav Kantheti <k@bk.bio>
2026-07-24 14:07:44 -0500

    ADD doc abstraction

--- crates/doc/src/lib.rs
+++ crates/doc/src/lib.rs
@@ -1,1 +1,32 @@
+pub const DOCS_HOME: &str = "https://www.palantir.com/docs/foundry/";
 
+pub struct Client {
+    http: reqwest::Client,
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum Domain {
+    Product,
+    Platform,
+    Updates,
+}
+
+impl Domain {
+    pub const ALL: [Self; 3] = [Self::Product, Self::Platform, Self::Updates];
+
+    pub const fn name(self) -> &'static str {
+        match self {
+            Self::Product => "product",
+            Self::Platform => "platform",
+            Self::Updates => "updates",
+        }
+    }
+
+    pub const fn description(self) -> &'static str {
+        match self {
+            Self::Product => "Palantir Foundry products' documentation",
+            Self::Platform => "Palantir Foundry platform-as-API reference",
+            Self::Updates => "Palantir Foundry platform updates",
+        }
+    }
+}
