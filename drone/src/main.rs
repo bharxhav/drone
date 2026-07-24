f931e47d6ebc17b6fd259d6fd0ff755fb114db99
Bhargav Kantheti <k@bk.bio>
2026-07-24 10:41:54 -0500

    INI cli with clap and sysexit

--- drone/src/main.rs
+++ drone/src/main.rs
@@ -1,1 +1,32 @@
+mod consts;
 
+use clap::{CommandFactory, Parser, Subcommand};
+use sysexits::ExitCode;
+
+use crate::consts::LOGO;
+
+#[derive(Parser)]
+#[command(version, about = "CLI for Palantir Foundry", before_help = LOGO)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Command>,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    /// Return the Palantir Foundry documentation.
+    Man,
+}
+
+fn main() -> ExitCode {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Some(Command::Man) => ExitCode::Ok,
+        None => {
+            Cli::command().print_help().expect("failed to print help");
+            println!();
+            ExitCode::Ok
+        }
+    }
+}
