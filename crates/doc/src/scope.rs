06dc44f3a4fcb17e7bf074f3e08a43a9bc0e5a8e
Bhargav Kantheti <k@bk.bio>
2026-07-25 10:58:06 -0500

    ADD const fns

--- crates/doc/src/scope.rs
+++ crates/doc/src/scope.rs
@@ -1,7 +1,42 @@
-///! Route that appends to https://www.palantir.com/docs/
-pub type Route = Vec<String>;
+use std::ops::Deref;
 
-///! Route that appends to [product | platform | updates]
-pub type Scope = Vec<String>;
+/// A path relative to `https://www.palantir.com/`.
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub(super) struct Route(Vec<String>);
 
-// Impl both Froms using Domain's methods.
+impl Route {
+    pub const fn base_url() -> &'static str {
+        "https://www.palantir.com/"
+    }
+}
+
+/// A client path beginning with `product`, `platform`, or `updates`.
+#[derive(Clone, Debug, Default, Eq, Hash, PartialEq)]
+pub struct Scope(Vec<String>);
+
+impl Scope {
+    pub const fn base_url() -> &'static str {
+        "https://www.palantir.com/docs/foundry/"
+    }
+}
+
+macro_rules! impl_path {
+    ($type:ty) => {
+        impl Deref for $type {
+            type Target = [String];
+
+            fn deref(&self) -> &Self::Target {
+                &self.0
+            }
+        }
+
+        impl From<Vec<String>> for $type {
+            fn from(segments: Vec<String>) -> Self {
+                Self(segments)
+            }
+        }
+    };
+}
+
+impl_path!(Route);
+impl_path!(Scope);
