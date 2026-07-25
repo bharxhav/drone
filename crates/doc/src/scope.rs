e4550c849d13d697a0a6cc11595443dbdc75907e
Bhargav Kantheti <k@bk.bio>
2026-07-25 10:24:00 -0500

    ADD routing colors

---
+++ crates/doc/src/scope.rs
@@ -0,0 +1,7 @@
+///! Route that appends to https://www.palantir.com/docs/
+pub type Route = Vec<String>;
+
+///! Route that appends to [product | platform | updates]
+pub type Scope = Vec<String>;
+
+// Impl both Froms using Domain's methods.
