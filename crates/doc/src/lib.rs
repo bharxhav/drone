8d1bde2d9b9dda24b55f9b5cbf51f6ff5d2ddb21
Bhargav Kantheti <k@bk.bio>
2026-07-25 09:34:06 -0500

    FIX domain leakage

--- crates/doc/src/lib.rs
+++ crates/doc/src/lib.rs
@@ -2,25 +2,37 @@ mod domain;
 mod error;
 mod response;
 
-pub use domain::Domain;
+use std::collections::HashMap;
+
 pub use error::Error;
 pub use response::*;
 
+use domain::Domain;
+
 pub struct Client {
     http: reqwest::Client,
+    cache: HashMap<Vec<String>, Documentation>,
 }
 
 impl Client {
     pub fn new() -> Self {
         Self {
             http: reqwest::Client::new(),
+            cache: HashMap::new(),
         }
     }
 
-    pub async fn get<S>(&self, domain: Domain, scope: &[S]) -> Result<Documentation, Error>
+    pub async fn get<S>(&self, scope: &[S]) -> Result<Documentation, Error>
     where
         S: AsRef<str>,
     {
+        let Some((domain, scope)) = scope.split_first() else {
+            return Ok(Domain::ALL.into());
+        };
+
+        let domain = Domain::from_scope(domain.as_ref())
+            .ok_or_else(|| Error::NotFound(domain.as_ref().to_owned()))?;
+
         domain.get(self, scope).await
     }
 }
