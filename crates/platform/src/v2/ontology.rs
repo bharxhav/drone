b136536bd78c4e63f3cf57e04611591636803182
Bhargav Kantheti <k@bk.bio>
2026-08-13 17:42:12 -0500

    ADD o discovery

--- crates/platform/src/v2/ontology.rs
+++ crates/platform/src/v2/ontology.rs
@@ -1,6 +1,7 @@
 use serde::Deserialize;
 
-use crate::Rid;
+use super::Client;
+use crate::{FoundryError, Rid};
 
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq)]
 #[serde(rename_all = "camelCase")]
@@ -9,4 +10,41 @@ pub struct Ontology {
     pub display_name: String,
     pub description: String,
     pub rid: Rid,
+}
+
+impl Client {
+    pub async fn list_ontologies(&self) -> Result<Vec<Ontology>, FoundryError> {
+        #[derive(Deserialize)]
+        struct Response {
+            data: Vec<Ontology>,
+        }
+
+        let url = self.hostname.join("api/v2/ontologies")?;
+        let response: Response = self
+            .http
+            .get(url)
+            .send()
+            .await?
+            .error_for_status()?
+            .json()
+            .await?;
+
+        Ok(response.data)
+    }
+
+    /// Fetches a single ontology by its API name or RID.
+    pub async fn get_ontology(&self, ontology: impl AsRef<str>) -> Result<Ontology, FoundryError> {
+        let url = self
+            .hostname
+            .join("api/v2/ontologies/")?
+            .join(ontology.as_ref())?;
+        self.http
+            .get(url)
+            .send()
+            .await?
+            .error_for_status()?
+            .json()
+            .await
+            .map_err(Into::into)
+    }
 }
