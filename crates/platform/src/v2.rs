b091864858a20bf21ea7f5d1bc69925996da523f
Bhargav Kantheti <k@bk.bio>
2026-08-13 15:05:39 -0500

    MV client

---
+++ crates/platform/src/v2.rs
@@ -0,0 +1,28 @@
+use reqwest::header::{HeaderMap, HeaderValue};
+use url::Url;
+
+use crate::FoundryError;
+
+pub mod filesystem;
+
+pub struct Client {
+    pub hostname: Url,
+    http: reqwest::Client,
+}
+
+impl Client {
+    pub fn new(hostname: &str, token: &str) -> Result<Self, FoundryError> {
+        let hostname = Url::parse(hostname)?;
+
+        let mut headers = HeaderMap::new();
+        let mut authorization = HeaderValue::from_str(&format!("Bearer {token}"))?;
+        authorization.set_sensitive(true);
+        headers.insert(reqwest::header::AUTHORIZATION, authorization);
+
+        let http = reqwest::Client::builder()
+            .default_headers(headers)
+            .build()?;
+
+        Ok(Self { hostname, http })
+    }
+}
