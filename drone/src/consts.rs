b62658ea7973c5f0bf41acece28d93cfafc8b75f
Bhargav Kantheti <k@bk.bio>
2026-08-19 09:46:24 -0500

    ADD authors

--- drone/src/consts.rs
+++ drone/src/consts.rs
@@ -1,1 +1,5 @@
 pub const LOGO: &str = "     ▄     \n  ▄▄███▄▄  \n▄█▀██ ██▀█▄\n▌   ▀ ▀   ▐\n     ▀     ";
+
+pub const APP_TOP_LEVEL_DOMAIN: &str = "bio";
+pub const APP_AUTHOR: &str = "bk";
+pub const APP_NAME: &str = "drone";
