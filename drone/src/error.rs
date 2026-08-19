4b58be81cc85d9f7871142cf4e50435b26fdd8c5
Bhargav Kantheti <k@bk.bio>
2026-08-19 09:50:33 -0500

    ADD error file

---
+++ drone/src/error.rs
@@ -0,0 +1,29 @@
+use std::io;
+
+use miette::Diagnostic;
+use sysexits::ExitCode;
+use thiserror::Error;
+
+#[derive(Debug, Diagnostic, Error)]
+pub enum Error {
+    #[error("{0}")]
+    #[diagnostic(code(drone::config))]
+    Config(String),
+
+    #[error("I/O error")]
+    #[diagnostic(code(drone::io))]
+    Io(#[from] io::Error),
+
+    #[error("invalid configuration")]
+    #[diagnostic(code(drone::config::toml))]
+    Toml(#[from] toml::de::Error),
+}
+
+impl Error {
+    pub fn exit_code(&self) -> ExitCode {
+        match self {
+            Self::Config(_) | Self::Toml(_) => ExitCode::Config,
+            Self::Io(_) => ExitCode::IoErr,
+        }
+    }
+}
