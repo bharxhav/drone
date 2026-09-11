2a8e5b8816992c1bd6fc9d1df6fc99fc9ecf005e
Bhargav Kantheti <k@bk.bio>
2026-09-11 14:17:17 -0500

    ADD query type

--- crates/foundry-types/src/ontology.rs
+++ crates/foundry-types/src/ontology.rs
@@ -5,6 +5,7 @@ pub mod object;
 pub mod object_property_data_type;
 pub mod object_set;
 pub mod object_type;
+pub mod query_type;
 pub mod value_type;
 
 use serde::{Deserialize, Serialize};
---
+++ crates/foundry-types/src/ontology/query_type.rs
@@ -0,0 +1,87 @@
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+
+use crate::Rid;
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct QueryType {
+    pub ontology_rid: Rid,
+    pub api_name: String,
+    pub description: Option<String>,
+    pub display_name: Option<String>,
+    pub parameters: HashMap<String, QueryParameter>,
+    pub output: QueryDataType,
+    pub rid: Rid,
+    pub version: String,
+    pub type_references: HashMap<String, QueryDataType>,
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct QueryParameter {
+    pub description: Option<String>,
+    pub data_type: QueryDataType,
+    pub required: bool,
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+#[serde(tag = "type", rename_all = "camelCase")]
+pub enum QueryDataType {
+    Date,
+    InterfaceObject {
+        interface_api_name: String,
+    },
+    Struct {
+        fields: Vec<QueryStructField>,
+    },
+    String,
+    Integer,
+    ThreeDimensionalAggregation,
+    Float,
+    Long,
+    Unsupported,
+    Attachment,
+    Array {
+        #[serde(rename = "subType")]
+        sub_type: Box<QueryDataType>,
+    },
+    ObjectSet {
+        object_api_name: String,
+    },
+    TwoDimensionalAggregation,
+    TypeReference {
+        type_id: String,
+    },
+    Timestamp,
+    Set {
+        #[serde(rename = "subType")]
+        sub_type: Box<QueryDataType>,
+    },
+    Void,
+    EntrySet {
+        key_type: Box<QueryDataType>,
+        value_type: Box<QueryDataType>,
+    },
+    Double,
+    Union {
+        union_types: Vec<QueryDataType>,
+    },
+    Boolean,
+    MediaReference,
+    Null,
+    InterfaceObjectSet {
+        interface_api_name: String,
+    },
+    Object {
+        object_api_name: String,
+    },
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct QueryStructField {
+    pub name: String,
+    pub field_type: QueryDataType,
+}
